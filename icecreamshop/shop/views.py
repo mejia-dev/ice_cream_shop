50bb3476c34a27915d254a339132aebd02d35fbf
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-21 14:04:23 -0700

    Render main index page

--- icecreamshop/shop/views.py
+++ icecreamshop/shop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+# from .models import Flavor, Treat
+
+def index(request):
+    return render(request, "shop/index.html",)
+
+# class FlavorsIndexView(generic.ListView):
+#     template_name = "shop/flavors/index.html"
+#     context_object_name = "flavors_list"
+
+#     def get_queryset(self):
+#         return Flavor.objects
+  
