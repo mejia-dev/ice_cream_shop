cd7ce77fde4908660255017cd7b3ba4147943f66
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-21 13:16:28 -0700

    Add flavor and treat models

--- icecreamshop/shop/models.py
+++ icecreamshop/shop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Flavor(models.Model):
+    flavor_name = models.CharField(max_length=100)
+    price = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.flavor_name
+    
+
+class Treat(models.Model):
+    treat_name = models.CharField(max_length=100)
+    price = models.IntegerField(default=5)
+    flavors = models.ManyToManyField(Flavor)
+
+    def __str__(self):
+        return self.treat_name
