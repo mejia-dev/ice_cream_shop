d914f49629321e835d0c6f25654adb83e73fc339
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-21 14:39:34 -0700

    Render flavors on page

--- icecreamshop/shop/urls.py
+++ icecreamshop/shop/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "shop"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("flavors/", views.FlavorsIndexView.as_view(),name="flavors")
 ]
